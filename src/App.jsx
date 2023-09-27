b3dd7a7eee907dff0f240f8f408e5ab65be4633c
Sruthik Koneti <sruthikkoneti@gmail.com>
2023-09-27 13:00:01 +0530

    basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,11 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import ErrorPage from "./pages/ErrorPage";
-import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Navbar />
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="*" element={<ErrorPage />} />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,75 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [weatherData, SetWeatherData] = useState({});
+  const [location, setLocation] = useState("");
 
-export default Home
+  const handleLocation=(event)=>{
+    console.log(event.target.value)
+    setLocation(event.target.value)
+  }
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=b1f51569f3a7bd49f7e8052e31eee12b`;
+
+  const getWeather=async()=>{
+    try{
+      const response= await axios.get(url)
+      SetWeatherData(response.data)
+      console.log(response.data)
+      setLocation("")
+    }catch(err){
+      console.log(err)
+    }
+  }
+
+  return (
+    <div className="app">
+      <div className="search">
+        <input
+          value={location}
+          onChange={handleLocation}
+          placeholder="Enter Location"
+          type="text"
+        />
+        <button onClick={getWeather}>search</button>
+      </div>
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>{weatherData.name}</p>
+          </div>
+          <div className="temp">
+            {weatherData.main ? <h1>{weatherData.main.temp.toFixed()}°F</h1> : null}
+          </div>
+          <div className="description">
+            {weatherData.weather ? <p>{weatherData.weather[0].main}</p> : null}
+          </div>
+        </div>
+
+        {weatherData.name !== undefined && (
+          <div className="bottom">
+            <div className="feels">
+              {weatherData.main ? (
+                <p className="bold">{weatherData.main.feels_like.toFixed()}°F</p>
+              ) : null}
+              <p>Feels Like</p>
+            </div>
+            <div className="humidity">
+              {weatherData.main ? <p className="bold">{weatherData.main.humidity}%</p> : null}
+              <p>Humidity</p>
+            </div>
+            <div className="wind">
+              {weatherData.wind ? (
+                <p className="bold">{weatherData.wind.speed.toFixed()} MPH</p>
+              ) : null}
+              <p>Wind Speed</p>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
